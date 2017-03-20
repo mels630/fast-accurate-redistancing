3f8f3170e2aff7a008a8eb82a995201d0fda5940
melsey <met>
2017-03-19 21:44:46 -0700

    Removed redundant code in idarray2d.cpp

--- idarray2d.cpp
+++ idarray2d.cpp
@@ -29,6 +29,73 @@ IDArray2D::~IDArray2D()
   freeQeIdxAll();
 }
 
+void IDArray2D::SetQe(idx_t const idx) const
+{
+  if (qe[idx] != nullptr)
+    return;
+  if (flag == 1) {
+    qe[idx] = new double [4];
+    qe[idx][0] = get(idx);
+    qe[idx][1] = getyp(idx);
+    qe[idx][2] = getxp(idx);
+    qe[idx][3] = getyp(xp(idx));
+  } else if (flag == 2) {
+    qe[idx] = new double [9];
+    double A[9];
+    A[0] = getxm(yp(idx)); A[1] = getxm(idx); A[2] = getxm(ym(idx));
+    A[3] = getyp(idx); A[4] = get(idx); A[5] = getym(idx);
+    A[6] = getxp(yp(idx)); A[7] = getxp(idx); A[8] = getxp(ym(idx));
+    qe[idx][0] = ((A[0]+A[2]+A[6]+A[8])-2.0*(A[1]+A[3]+A[5]+A[7])+4.0*A[4])/4.0;
+    qe[idx][1] = ((A[0]-A[2]+A[6]-A[8])+2.0*(A[5]-A[3]))/4.0;
+    qe[idx][2] = (A[1]-2.0*A[4]+A[7])/2.0;
+    qe[idx][3] = ((-A[0]-A[2]+A[6]+A[8])+2.0*(A[1]-A[7]))/4.0;
+    qe[idx][4] = (-A[0]+A[2]+A[6]-A[8])/4.0;
+    qe[idx][5] = (-A[1]+A[7])/2.0;
+    qe[idx][6] = (A[3]-2.0*A[4]+A[5])/2.0;
+    qe[idx][7] = (A[3]-A[5])/2.0;
+    qe[idx][8] = A[4];
+  } else { // flag == 3
+    qe[idx] = new double [16];
+    double A[16];
+    A[ 0] = getxm(ym(idx));
+    A[ 1] = getxm(idx);
+    A[ 2] = getxm(yp(idx));
+    A[ 3] = getxm(yp(yp(idx)));
+
+    A[ 4] = getym(idx);
+    A[ 5] = get(idx);
+    A[ 6] = getyp(idx);
+    A[ 7] = getyp(yp(idx));
+
+    A[ 8] = getxp(ym(idx));
+    A[ 9] = getxp(idx);
+    A[10] = getxp(yp(idx));
+    A[11] = getxp(yp(yp(idx)));
+
+    A[12] = getxp(xp(ym(idx)));
+    A[13] = getxp(xp(idx));
+    A[14] = getxp(xp(yp(idx)));
+    A[15] = getxp(xp(yp(yp(idx))));
+
+    qe[idx][0] = A[5];
+    qe[idx][1] = -A[7]/6.0+A[6]-A[5]/2.0-A[4]/3.0;
+    qe[idx][2] = (A[6]+A[4])/2.0 - A[5];
+    qe[idx][3] = (A[7]-A[4])/6.0 - (A[6]-A[5])/2.0;
+    qe[idx][4] = -A[13]/6.0+A[9]-A[5]/2.0-A[1]/3.0;
+    qe[idx][5] = A[15]/36.0 + (A[12]+A[3])/18.0 + (A[13]+A[7])/12.0 + A[0]/9.0 + (-A[14]-A[11]+A[4]+A[1])/6.0 + A[5]/4.0 - (A[8]+A[2])/3.0 - (A[9]+A[6])/2.0 + A[10];
+    qe[idx][6] = -(A[14]+A[12])/12.0 - (A[2]-A[13]+A[0])/6.0 - (A[6]+A[4])/4.0 + A[1]/3.0 + (A[10]+A[5]+A[8])/2.0 - A[9]; 
+    qe[idx][7] = (-A[15]+A[12])/36.0 + (-A[3]+A[0])/18.0 + (-A[7]+A[14]-A[13]+A[4])/12.0 + (A[11]+A[2]-A[1]-A[8])/6.0 + (A[6]-A[5])/4.0 + (-A[10]+A[9])/2.0;
+    qe[idx][8] = (A[9]+A[1])/2.0 - A[5];
+    qe[idx][9] = -(A[11]+A[3])/12.0 - (A[8]-A[7]+A[0])/6.0 - (A[9]+A[1])/4.0 + A[4]/3.0 + (A[10]+A[5]+A[2])/2.0 - A[6];
+    qe[idx][10] = (A[10]+A[8]+A[2]+A[0])/4.0 - (A[9]+A[6]+A[4]+A[1])/2.0 + A[5];
+    qe[idx][11] = (A[11]-A[8]+A[3]-A[0])/12.0 - (A[10]-A[9]+A[2]-A[1])/4.0 - (A[7]-A[4])/6.0 + (A[6]-A[5])/2.0;
+    qe[idx][12] = (A[13]-A[1])/6.0 - (A[9]-A[5])/2.0;
+    qe[idx][13] = (-A[15]+A[3])/36.0 + (-A[12]+A[0])/18.0 + (-A[13]+A[11]-A[7]+A[1])/12.0 + (A[14]+A[8]-A[4]-A[2])/6.0 + (A[9]-A[5])/4.0 + (-A[10]+A[6])/2.0;
+    qe[idx][14] = (A[14]-A[2]+A[12]-A[0])/12.0 - (A[10]-A[6]+A[8]-A[4])/4.0 - (A[13]-A[1])/6.0 + (A[9]-A[5])/2.0;
+    qe[idx][15] = (A[15]-A[12]-A[3]+A[0])/36.0 - (A[14]-A[13]+A[11]-A[8]-A[7]+A[4]-A[2]+A[1])/12.0 + (A[10]-A[9]-A[6]+A[5])/4.0;
+  }
+}
+
 double IDArray2D::interpolate(double xx, double yy) const
 /*
 {
@@ -53,16 +120,10 @@ double IDArray2D::interpolate(double xx, double yy) const
     const idx_t jj = static_cast<idx_t>(floor(x*static_cast<double>(n)));
     const idx_t idx = inrange(ii,m) + inrange(jj,n)*m;
 
+    if (qe[idx] == nullptr)
+      SetQe(idx);
     const double rx = x*static_cast<double>(n)-static_cast<double>(jj); // in range [-1/2,1/2], not scaled by dx -- cancels with terms in qe matrix   
     const double ry = y*static_cast<double>(m)-static_cast<double>(ii);
-    if(qe[idx] == NULL)
-    { /* need to compute coefficients */
-      qe[idx] = new double [4];
-      qe[idx][0] = this->get(idx);
-      qe[idx][1] = this->getyp(idx);
-      qe[idx][2] = this->getxp(idx);
-      qe[idx][3] = this->getyp(this->xp(idx));
-    }
     const double xi1 = qe[idx][0]*(1.0-rx) + qe[idx][2]*rx;
     const double xi2 = qe[idx][1]*(1.0-rx) + qe[idx][3]*rx;
     return(xi1*(1.0-ry) + xi2*ry);
@@ -74,24 +135,8 @@ double IDArray2D::interpolate(double xx, double yy) const
     idx_t idx = inrange(ii,m) + inrange(jj,n)*m;                     // nearest grid point 
     double rx = x/dx-static_cast<double>(jj);; // in range [-1/2,1/2], not scaled by dx -- cancels with terms in qe matrix 
     double ry = y/dy-static_cast<double>(ii);  // note: this relies on ii being able to take values 0 OR m and only being cast into range for idx 
-
-    if(qe[idx] == NULL)
-    { // need to compute coefficients 
-      qe[idx] = new double [9];
-      double A[9];
-      A[0] = this->getxm(this->yp(idx)); A[1] = this->getxm(idx); A[2] = this->getxm(this->ym(idx));
-      A[3] = this->getyp(idx); A[4] = this->get(idx); A[5] = this->getym(idx);
-      A[6] = this->getxp(this->yp(idx)); A[7] = this->getxp(idx); A[8] = this->getxp(this->ym(idx));
-      qe[idx][0] = ((A[0]+A[2]+A[6]+A[8])-2.0*(A[1]+A[3]+A[5]+A[7])+4.0*A[4])/4.0;
-      qe[idx][1] = ((A[0]-A[2]+A[6]-A[8])+2.0*(A[5]-A[3]))/4.0;
-      qe[idx][2] = (A[1]-2.0*A[4]+A[7])/2.0;
-      qe[idx][3] = ((-A[0]-A[2]+A[6]+A[8])+2.0*(A[1]-A[7]))/4.0;
-      qe[idx][4] = (-A[0]+A[2]+A[6]-A[8])/4.0;
-      qe[idx][5] = (-A[1]+A[7])/2.0;
-      qe[idx][6] = (A[3]-2.0*A[4]+A[5])/2.0;
-      qe[idx][7] = (A[3]-A[5])/2.0;
-      qe[idx][8] = A[4];
-    }
+    if (qe[idx] == nullptr)
+      SetQe(idx);
     return( qe[idx][0]*rx*rx*ry*ry + qe[idx][1]*rx*rx*ry + qe[idx][2]*rx*rx+
             qe[idx][3]*rx*ry*ry + qe[idx][4]*rx*ry + qe[idx][5]*rx+
             qe[idx][6]*ry*ry + qe[idx][7]*ry + qe[idx][8] );
@@ -108,47 +153,8 @@ double IDArray2D::interpolate(double xx, double yy) const
     double rx2 = rx*rx; double rx3 = rx2*rx;
     double ry = (y/dy-static_cast<double>(ii)); // in range (-1,1), not scaled by dx/dy (picked up in coefficients) 
     double ry2 = ry*ry; double ry3 = ry2*ry;
-    if(qe[idx] == NULL)
-    {
-      qe[idx] = new double [16];
-      double A[16];
-      A[ 0] = this->getxm(this->ym(idx));
-      A[ 1] = this->getxm(idx);
-      A[ 2] = this->getxm(this->yp(idx));
-      A[ 3] = this->getxm(this->yp(this->yp(idx)));
-
-      A[ 4] = this->getym(idx);
-      A[ 5] = this->get(idx);
-      A[ 6] = this->getyp(idx);
-      A[ 7] = this->getyp(this->yp(idx));
-
-      A[ 8] = this->getxp(this->ym(idx));
-      A[ 9] = this->getxp(idx);
-      A[10] = this->getxp(this->yp(idx));
-      A[11] = this->getxp(this->yp(this->yp(idx)));
-
-      A[12] = this->getxp(this->xp(this->ym(idx)));
-      A[13] = this->getxp(this->xp(idx));
-      A[14] = this->getxp(this->xp(this->yp(idx)));
-      A[15] = this->getxp(this->xp(this->yp(this->yp(idx))));
-
-      qe[idx][0] = A[5];
-      qe[idx][1] = -A[7]/6.0+A[6]-A[5]/2.0-A[4]/3.0;
-      qe[idx][2] = (A[6]+A[4])/2.0 - A[5];
-      qe[idx][3] = (A[7]-A[4])/6.0 - (A[6]-A[5])/2.0;
-      qe[idx][4] = -A[13]/6.0+A[9]-A[5]/2.0-A[1]/3.0;
-      qe[idx][5] = A[15]/36.0 + (A[12]+A[3])/18.0 + (A[13]+A[7])/12.0 + A[0]/9.0 + (-A[14]-A[11]+A[4]+A[1])/6.0 + A[5]/4.0 - (A[8]+A[2])/3.0 - (A[9]+A[6])/2.0 + A[10];
-      qe[idx][6] = -(A[14]+A[12])/12.0 - (A[2]-A[13]+A[0])/6.0 - (A[6]+A[4])/4.0 + A[1]/3.0 + (A[10]+A[5]+A[8])/2.0 - A[9]; 
-      qe[idx][7] = (-A[15]+A[12])/36.0 + (-A[3]+A[0])/18.0 + (-A[7]+A[14]-A[13]+A[4])/12.0 + (A[11]+A[2]-A[1]-A[8])/6.0 + (A[6]-A[5])/4.0 + (-A[10]+A[9])/2.0;
-      qe[idx][8] = (A[9]+A[1])/2.0 - A[5];
-      qe[idx][9] = -(A[11]+A[3])/12.0 - (A[8]-A[7]+A[0])/6.0 - (A[9]+A[1])/4.0 + A[4]/3.0 + (A[10]+A[5]+A[2])/2.0 - A[6];
-      qe[idx][10] = (A[10]+A[8]+A[2]+A[0])/4.0 - (A[9]+A[6]+A[4]+A[1])/2.0 + A[5];
-      qe[idx][11] = (A[11]-A[8]+A[3]-A[0])/12.0 - (A[10]-A[9]+A[2]-A[1])/4.0 - (A[7]-A[4])/6.0 + (A[6]-A[5])/2.0;
-      qe[idx][12] = (A[13]-A[1])/6.0 - (A[9]-A[5])/2.0;
-      qe[idx][13] = (-A[15]+A[3])/36.0 + (-A[12]+A[0])/18.0 + (-A[13]+A[11]-A[7]+A[1])/12.0 + (A[14]+A[8]-A[4]-A[2])/6.0 + (A[9]-A[5])/4.0 + (-A[10]+A[6])/2.0;
-      qe[idx][14] = (A[14]-A[2]+A[12]-A[0])/12.0 - (A[10]-A[6]+A[8]-A[4])/4.0 - (A[13]-A[1])/6.0 + (A[9]-A[5])/2.0;
-      qe[idx][15] = (A[15]-A[12]-A[3]+A[0])/36.0 - (A[14]-A[13]+A[11]-A[8]-A[7]+A[4]-A[2]+A[1])/12.0 + (A[10]-A[9]-A[6]+A[5])/4.0;
-    }
+    if (qe[idx] == nullptr)
+      SetQe(idx);
     return ((qe[idx][0]+qe[idx][1]*ry+qe[idx][2]*ry2+qe[idx][3]*ry3)+rx*(qe[idx][4]+qe[idx][5]*ry+qe[idx][6]*ry2+qe[idx][7]*ry3)+rx2*(qe[idx][8]+qe[idx][9]*ry+qe[idx][10]*ry2+qe[idx][11]*ry3)+rx3*(qe[idx][12]+qe[idx][13]*ry+qe[idx][14]*ry2+qe[idx][15]*ry3));
   }
 }
@@ -182,24 +188,9 @@ void IDArray2D::interpolate(double xx, double yy, double(&result)[3]) const // r
     const double ry = y/dy-static_cast<double>(ii);  // note: this relies on ii being able to take values 0 OR m and only being cast into range for idx 
     const double rx2 = rx*rx;
     const double ry2 = ry*ry;
+    if (qe[idx] == nullptr)
+      SetQe(idx);
 
-    if(qe[idx] == NULL)
-    { // need to compute coefficients 
-      qe[idx] = new double [9];
-      double A[9];
-      A[0] = this->getxm(this->yp(idx)); A[1] = this->getxm(idx); A[2] = this->getxm(this->ym(idx));
-      A[3] = this->getyp(idx); A[4] = this->get(idx); A[5] = this->getym(idx);
-      A[6] = this->getxp(this->yp(idx)); A[7] = this->getxp(idx); A[8] = this->getxp(this->ym(idx));
-      qe[idx][0] = ((A[0]+A[2]+A[6]+A[8])-2.0*(A[1]+A[3]+A[5]+A[7])+4.0*A[4])/4.0;
-      qe[idx][1] = ((A[0]-A[2]+A[6]-A[8])+2.0*(A[5]-A[3]))/4.0;
-      qe[idx][2] = (A[1]-2.0*A[4]+A[7])/2.0;
-      qe[idx][3] = ((-A[0]-A[2]+A[6]+A[8])+2.0*(A[1]-A[7]))/4.0;
-      qe[idx][4] = (-A[0]+A[2]+A[6]-A[8])/4.0;
-      qe[idx][5] = (-A[1]+A[7])/2.0;
-      qe[idx][6] = (A[3]-2.0*A[4]+A[5])/2.0;
-      qe[idx][7] = (A[3]-A[5])/2.0;
-      qe[idx][8] = A[4];
-    }
     result[0] = ( qe[idx][0]*rx2*ry2 + qe[idx][1]*rx2*ry + qe[idx][2]*rx2+
                   qe[idx][3]*rx*ry2 + qe[idx][4]*rx*ry + qe[idx][5]*rx+
                   qe[idx][6]*ry2 + qe[idx][7]*ry + qe[idx][8] );
@@ -219,47 +210,8 @@ void IDArray2D::interpolate(double xx, double yy, double(&result)[3]) const // r
     const double rx2 = rx*rx; const double rx3 = rx2*rx;
     const double ry = (y*static_cast<double>(m)-static_cast<double>(ii)); // in range (-1,1), not scaled by dx/dy (picked up in coefficients) 
     const double ry2 = ry*ry; const double ry3 = ry2*ry;
-    if(qe[idx] == NULL)
-    {
-      qe[idx] = new double [16];
-      double A[16];
-      A[ 0] = this->getxm(this->ym(idx));
-      A[ 1] = this->getxm(idx);
-      A[ 2] = this->getxm(this->yp(idx));
-      A[ 3] = this->getxm(this->yp(this->yp(idx)));
-
-      A[ 4] = this->getym(idx);
-      A[ 5] = this->get(idx);
-      A[ 6] = this->getyp(idx);
-      A[ 7] = this->getyp(this->yp(idx));
-
-      A[ 8] = this->getxp(this->ym(idx));
-      A[ 9] = this->getxp(idx);
-      A[10] = this->getxp(this->yp(idx));
-      A[11] = this->getxp(this->yp(this->yp(idx)));
-
-      A[12] = this->getxp(this->xp(this->ym(idx)));
-      A[13] = this->getxp(this->xp(idx));
-      A[14] = this->getxp(this->xp(this->yp(idx)));
-      A[15] = this->getxp(this->xp(this->yp(this->yp(idx))));
-
-      qe[idx][0] = A[5];
-      qe[idx][1] = -A[7]/6.0+A[6]-A[5]/2.0-A[4]/3.0;
-      qe[idx][2] = (A[6]+A[4])/2.0 - A[5];
-      qe[idx][3] = (A[7]-A[4])/6.0 - (A[6]-A[5])/2.0;
-      qe[idx][4] = -A[13]/6.0+A[9]-A[5]/2.0-A[1]/3.0;
-      qe[idx][5] = A[15]/36.0 + (A[12]+A[3])/18.0 + (A[13]+A[7])/12.0 + A[0]/9.0 + (-A[14]-A[11]+A[4]+A[1])/6.0 + A[5]/4.0 - (A[8]+A[2])/3.0 - (A[9]+A[6])/2.0 + A[10];
-      qe[idx][6] = -(A[14]+A[12])/12.0 - (A[2]-A[13]+A[0])/6.0 - (A[6]+A[4])/4.0 + A[1]/3.0 + (A[10]+A[5]+A[8])/2.0 - A[9]; 
-      qe[idx][7] = (-A[15]+A[12])/36.0 + (-A[3]+A[0])/18.0 + (-A[7]+A[14]-A[13]+A[4])/12.0 + (A[11]+A[2]-A[1]-A[8])/6.0 + (A[6]-A[5])/4.0 + (-A[10]+A[9])/2.0;
-      qe[idx][8] = (A[9]+A[1])/2.0 - A[5];
-      qe[idx][9] = -(A[11]+A[3])/12.0 - (A[8]-A[7]+A[0])/6.0 - (A[9]+A[1])/4.0 + A[4]/3.0 + (A[10]+A[5]+A[2])/2.0 - A[6];
-      qe[idx][10] = (A[10]+A[8]+A[2]+A[0])/4.0 - (A[9]+A[6]+A[4]+A[1])/2.0 + A[5];
-      qe[idx][11] = (A[11]-A[8]+A[3]-A[0])/12.0 - (A[10]-A[9]+A[2]-A[1])/4.0 - (A[7]-A[4])/6.0 + (A[6]-A[5])/2.0;
-      qe[idx][12] = (A[13]-A[1])/6.0 - (A[9]-A[5])/2.0;
-      qe[idx][13] = (-A[15]+A[3])/36.0 + (-A[12]+A[0])/18.0 + (-A[13]+A[11]-A[7]+A[1])/12.0 + (A[14]+A[8]-A[4]-A[2])/6.0 + (A[9]-A[5])/4.0 + (-A[10]+A[6])/2.0;
-      qe[idx][14] = (A[14]-A[2]+A[12]-A[0])/12.0 - (A[10]-A[6]+A[8]-A[4])/4.0 - (A[13]-A[1])/6.0 + (A[9]-A[5])/2.0;
-      qe[idx][15] = (A[15]-A[12]-A[3]+A[0])/36.0 - (A[14]-A[13]+A[11]-A[8]-A[7]+A[4]-A[2]+A[1])/12.0 + (A[10]-A[9]-A[6]+A[5])/4.0;
-    }
+    if (qe[idx] == nullptr)
+      SetQe(idx);
     result[0] = ((qe[idx][0]+qe[idx][1]*ry+qe[idx][2]*ry2+qe[idx][3]*ry3)+rx*(qe[idx][4]+qe[idx][5]*ry+qe[idx][6]*ry2+qe[idx][7]*ry3)+rx2*(qe[idx][8]+qe[idx][9]*ry+qe[idx][10]*ry2+qe[idx][11]*ry3)+rx3*(qe[idx][12]+qe[idx][13]*ry+qe[idx][14]*ry2+qe[idx][15]*ry3));
     result[1] = ((qe[idx][4]+qe[idx][5]*ry+qe[idx][6]*ry2+qe[idx][7]*ry3)+2.0f*rx*(qe[idx][8]+qe[idx][9]*ry+qe[idx][10]*ry2+qe[idx][11]*ry3)+3.0f*rx2*(qe[idx][12]+qe[idx][13]*ry+qe[idx][14]*ry2+qe[idx][15]*ry3))*dx;
     result[2] = ((qe[idx][ 1]+qe[idx][ 2]*2.0f*ry+qe[idx][ 3]*3.0f*ry2)
--- idarray2d.hpp
+++ idarray2d.hpp
@@ -5,22 +5,22 @@
 
 class IDArray2D : public Array2D<double> {
 private:
-  explicit IDArray2D();                             // Empty constructor not permitted
 
   mutable std::vector<double*> qe; // The entries of the vector are pointers to arrays containing
                                    // the interpolation coeffections for that index of the Array2D<double> data
                                    // These are only filled in once interpolation is asked for around the index.
-  const int flag;                  // Interpolation type: (1=bilinear, 2=biquadratic, 3=bicubic)
-public:
-  explicit IDArray2D(const int nn, const int _flag);                 // Constructor for nn x nn array
-  explicit IDArray2D(const int mm, const int nn, const int _flag);   // Constructor for mm x nn array
-  explicit IDArray2D(const int mm, const int nn, const double _dx, const double _dy, const int _flag);   // Constructor for mm x nn array
-  explicit IDArray2D(const Array2D<double> &input, const int _flag); // Constructor to deep copy "input" and initialize qe
-  ~IDArray2D();                                     // Destructor deallocates memory allocated in entries of qe
-  double interpolate(const double xx, double yy) const; // returns interpolated value of data(xx,yy), where data is imagined as a discretization of [0,1)^2
-  void interpolate(const double xx, const double yy, double(&result)[3]) const; // returns interpolated value of data(xx,yy) in result[0] and gradient of data(x,y) in result[1:2]
-  void freeQeIdx(const int idx) const; // clear interpolation coefficients at idx
+  int const flag;                  // Interpolation type: (1=bilinear, 2=biquadratic, 3=bicubic)
+  void SetQe(idx_t const idx) const;
   void freeQeIdxAll() const; // clear all interpolation coefficients
+public:
+  IDArray2D() = delete; // Empty constructor not permitted
+  IDArray2D(int const nn, int const _flag);                 // Constructor for nn x nn array
+  IDArray2D(int const mm, int const nn, int const _flag);   // Constructor for mm x nn array
+  IDArray2D(int const mm, int const nn, double const _dx, double const _dy, int const _flag);   // Constructor for mm x nn array
+  IDArray2D(const Array2D<double> &input, int const _flag); // Constructor to deep copy "input" and initialize qe
+  ~IDArray2D();                                     // Destructor deallocates memory allocated in entries of qe
+  double interpolate(double x, double y) const; // returns interpolated value of data(xx,yy), where data is imagined as a discretization of [0,1)^2
+  void interpolate(double x, double y, double(&result)[3]) const; // returns interpolated value of data(xx,yy) in result[0] and gradient of data(x,y) in result[1:2]
 };
 
 #endif
